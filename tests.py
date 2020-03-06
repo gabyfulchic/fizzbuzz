21b2a1b522f8c19cf49c333a6c27ada174da029d
gfulchic <gfulchic@deezer.com>
2020-03-06 15:34:44 +0100

    ADD - all tests are ok now

--- tests.py
+++ tests.py
@@ -9,6 +9,10 @@ def check_X_divisible_by_3(fizz):
     modulo = 3 % fizz
     assert modulo == 0
 
+def check_X_divisible_by_5(buzz):
+    modulo = 5 % buzz
+    assert modulo == 0
+
 def check_X_divisible_by_3_and_5(fizzbuzz):
     for i in fizzbuzz:
         modulo = 15 % i
@@ -19,6 +23,7 @@ def launch_tests(fizz, buzz):
     check_X_not_divisible_by_3_and_5(fizzbuzz)
     check_X_divisible_by_3_and_5(fizzbuzz)
     check_X_divisible_by_3(fizz)
+    check_X_divisible_by_5(buzz)
 
 fizz = 3
 buzz = 5
