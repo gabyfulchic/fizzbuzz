f10647dabda5c7003eb2e09c83981d7c37bdf682
gfulchic <gfulchic@deezer.com>
2020-03-06 15:31:39 +0100

    ADD - Red part for tests divisible by both values

--- tests.py
+++ tests.py
@@ -5,9 +5,15 @@ def check_X_not_divisible_by_3_and_5(fizzbuzz):
         modulo = 2 % i
         assert modulo != 0
 
+def check_X_divisible_by_3_and_5(fizzbuzz):
+    for i in fizzbuzz:
+        modulo = 15 % i
+        assert modulo == 0
+
 def launch_tests(fizz, buzz):
     fizzbuzz = [fizz, buzz]
     check_X_not_divisible_by_3_and_5(fizzbuzz)
+    check_X_divisible_by_3_and_5(fizzbuzz)
 
 fizz = 3
 buzz = 5
